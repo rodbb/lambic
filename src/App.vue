9b138690067aa385d0c85155a51ea2e0303a36ee
amderbar <amderbar@gmail.com>
2019-02-10 16:58:01 +0900

    サイドメニューを開閉できるようにした #21

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app fixed v-model="permanent">
       <v-list>
         <v-list-tile>
           <v-list-tile-title class="title">
-            Lambic
+            <router-link :to="{ name: 'home' }">Lambic</router-link>
           </v-list-tile-title>
         </v-list-tile>
         <v-divider></v-divider>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar app color="light-green">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="permanent = !permanent"></v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer app></v-footer>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      permanent: false
+    }
+  }
+}
+</script>
