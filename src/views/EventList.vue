b24a00f76042f26d020e99e014d90e581fd05ff9
Y-Kanoh <headgcf.es@gmail.com>
2019-02-19 22:55:45 +0900

    参加者用/ビアバッシュ一覧機能
    コンポーネントを追加してみる
    refs #2

---
+++ src/views/EventList.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h1 class="headline">イベント一覧</h1>
+    </v-card-title>
+  </v-card>
+
+</template>
+<script>
+export default {
+  name: 'events',
+  computed: {
+    events () {
+      return this.$store.getters.events
+    }
+  }
+}
+</script>
