f4519d844f5b4ff97424670f464e33cd7e5b6807
Y-Kanoh <headgcf.es@gmail.com>
2019-02-24 22:31:29 +0900

    参加者用/ビアバッシュ一覧機能
    Home画面を消してしまっていたのでとりあえず復活。
    便宜上、Home画面にイベント一覧のリンクを追加
    refs #2

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './views/Home.vue'
 import EventList from './views/EventList.vue'
 import PresentationDetail from './views/PresentationDetail.vue'
 
@@ -7,6 +8,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
     {
       path: '/events',
       name: 'eventList',
