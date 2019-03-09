4b42d404efd7a5b403b7fee8f95ab5149713cbcc
Y-Kanoh <headgcf.es@gmail.com>
2019-03-09 14:26:38 +0900

    ページ遷移時にページの先頭が表示されない
    ページの先頭が表示されるよう修正。
    位置が保存されている場合はその場所を表示するように修正。
    refs #48

--- src/router.js
+++ src/router.js
@@ -38,5 +38,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
