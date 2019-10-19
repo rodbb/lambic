da01de8bbaae4bf2685d874b6766e71754aa0a88
skaji18 <kasuke18@gmail.com>
2019-10-19 08:10:08 +0000

    add puppeteer

---
+++ tests/integration/sample.spec.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai')
+
+describe('sample test', function () {
+  let page
+
+  before (async function () {
+    page = await browser.newPage()
+
+    page.on('console', msg => {
+      console.log('console: ', msg)
+    })
+
+    await page.goto('https://lambic-testing.firebaseapp.com')
+    await page.waitForSelector('.application--wrap')
+  })
+
+  after (async function () {
+    await page.close()
+  })
+
+  describe('check content on first dislpay' , function () {
+    it('check title', async function () {
+      expect(await page.title()).to.eql('Lambic')
+    })
+  
+    it('check userName', async function () {
+      var userName = await page.$eval('#userName', item => {
+          return item.textContent
+      })
+      expect(await userName.toString()).to.eql('ゲストユーザ')
+    })
+  })
+})
